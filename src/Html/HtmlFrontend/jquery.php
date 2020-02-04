afce9a086ade14da1e8c30564d6918f7ee19f805
macosx <nguyendinhcuong@gmail.com>
2020-02-04 14:50:38 +0700

    fix core

--- src/Html/HtmlFrontend/jquery.php
+++ src/Html/HtmlFrontend/jquery.php
@@ -258,19 +258,20 @@ JS
         return;
     }
 
-    public static function fontawesome($debug = null)
-    {
-        // Include jQuery
-        static::framework();
-        // If no debugging value is set, use the configuration setting
-        // Only attempt to load the component if it's supported in core and hasn't already been loaded
-        if (empty(static::$loaded[__METHOD__])) {
-            $doc = Factory::getDocument();
-            $doc->addStyleSheet('resources/js/fontawesome-free-5.11.2/css/all.min.css');
-            static::$loaded[__METHOD__] = true;
-        }
-        return;
-    }
+	public static function fontawesome($debug = null)
+	{
+
+		// Include jQuery
+		static::framework();
+		// If no debugging value is set, use the configuration setting
+		// Only attempt to load the component if it's supported in core and hasn't already been loaded
+		if (empty(static::$loaded[__METHOD__])) {
+			$doc = Factory::getDocument();
+			$doc->addStyleSheet('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css');
+			static::$loaded[__METHOD__] = true;
+		}
+		return;
+	}
     public static function icheck($debug = null)
     {
         // Include jQuery
@@ -311,20 +312,20 @@ JS
         }
         return;
     }
-    public static function bootstrap($debug = null)
-    {
-        // Include jQuery
-        static::framework();
-        // If no debugging value is set, use the configuration setting
-        // Only attempt to load the component if it's supported in core and hasn't already been loaded
-        if (empty(static::$loaded[__METHOD__])) {
-            $doc = Factory::getDocument();
-            $doc->addStyleSheet('resources/js/bootstrap-3.3.7-dist/css/bootstrap.css');
-            $doc->addScript('resources/js/bootstrap-3.3.7-dist/js/bootstrap.js');
-            static::$loaded[__METHOD__] = true;
-        }
-        return;
-    }
+	public static function bootstrap($debug = null)
+	{
+		// Include jQuery
+		static::framework();
+		// If no debugging value is set, use the configuration setting
+		// Only attempt to load the component if it's supported in core and hasn't already been loaded
+		if (empty(static::$loaded[__METHOD__])) {
+			$doc = Factory::getDocument();
+			$doc->addStyleSheet('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+			$doc->addScript('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+			static::$loaded[__METHOD__] = true;
+		}
+		return;
+	}
     public static function tooltip($debug = null)
     {
         // Include jQuery
