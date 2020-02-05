e0e308d5710c0b984c8ad6a8ff4fce626fb74c81
macosx <nguyendinhcuong@gmail.com>
2020-02-05 09:30:17 +0700

    setup cdn

--- src/opensource/WordPress/WooBookingOnWordpress.php
+++ src/opensource/WordPress/WooBookingOnWordpress.php
@@ -560,7 +560,7 @@ class WooBookingOnWordpress
 
 			// This is an example of how to include a plugin bundled with a theme.
 			array(
-				'name' => 'woopanel',
+				'name' => 'Woocommerce dashboard',
 				// The plugin name.
 				'slug' => 'woopanel',
 				// The plugin slug (typically the folder name).
@@ -574,7 +574,6 @@ class WooBookingOnWordpress
 				// If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false,
 				// If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader',
 				// If set, overrides default API URL and points to an external URL.
 				'is_callable' => '',
 				// If set, this callable will be be checked for availability to determine if a plugin is active.
@@ -594,33 +593,10 @@ class WooBookingOnWordpress
 				// If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false,
 				// If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader',
-				// If set, overrides default API URL and points to an external URL.
-				'is_callable' => '',
-				// If set, this callable will be be checked for availability to determine if a plugin is active.
-			),
-			array(
-				'name' => 'dokan',
-				// The plugin name.
-				'slug' => 'dokan',
-				// The plugin slug (typically the folder name).
-				'source' => 'https://downloads.wordpress.org/plugin/dokan-lite.2.9.30.zip',
-				// The plugin source.
-				'required' => true,
-				// If false, the plugin is only 'recommended' instead of required.
-				'version' => '',
-				// E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-				'force_activation' => false,
-				// If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-				'force_deactivation' => false,
-				// If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-				'external_url' => 'https://github.com/thomasgriffin/New-Media-Image-Uploader',
 				// If set, overrides default API URL and points to an external URL.
 				'is_callable' => '',
 				// If set, this callable will be be checked for availability to determine if a plugin is active.
 			)
-
-
 		);
 
 		/*
